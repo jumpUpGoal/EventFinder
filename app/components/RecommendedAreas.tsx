9f193cad07901ba6299ae80dd8913ebdbd4755e3
jumpUpGoal <samuelgiampiccolo7866@gmail.com>
2024-09-23 14:29:34 +0700

    feat : update component

---
+++ app/components/RecommendedAreas.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const recommendedAreas = [
+  {
+    address: "Scotiabank Arena",
+    lon: -79.3791035,
+    lat: 43.6433895,
+  },
+  {
+    address: "Ontario Place",
+    lon: -79.41511374999999,
+    lat: 43.62939075,
+  },
+  {
+    address: "Danforth Music Hall",
+    lon: -79.357071,
+    lat: 43.676338,
+  },
+];
+
+export default function RecommendedAreas() {
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    // Initialize map here if needed
+  }, []);
+
+  const handleAreaClick = (lon, lat) => {
+    if (map) {
+      map.flyTo({
+        center: [lon, lat],
+        zoom: 15,
+      });
+    }
+  };
+
+  return (
+    <section className="my-3 rounded-xl border px-2 py-3 shadow-md shadow-blue-500/40">
+      <h2 className="font-pop text-2xl lg:text-3xl font-bold text-white my-3">
+        Recommended Areas
+      </h2>
+      <hr />
+      <ul id="recommended-areas">
+        {recommendedAreas.map((area, index) => (
+          <li
+            key={index}
+            className="text-xl cursor-pointer hover:text-cyan-700"
+            onClick={() => handleAreaClick(area.lon, area.lat)}>
+            {area.address}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
